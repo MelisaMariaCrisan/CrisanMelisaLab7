6a62f46771a8a5bc64f08722f33bd4c39e7b71b2
MELISA-MARIA CRIȘAN <melisa.crisan@stud.ubbcluj.ro>
2024-11-25 16:32:02 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace CrisanMelisaLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CrisanMelisaLab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
